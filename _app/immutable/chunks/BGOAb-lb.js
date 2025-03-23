497e2903746c3422c940ca87b7eab2e7c93cf951
nathanfaucett <nathanfaucett@users.noreply.github.com>
2025-03-23 09:33:19 +0000

    deploy: f1e73969e08ac2d98bd56db9342a001525ce9461

--- _app/immutable/chunks/BGOAb-lb.js
+++ _app/immutable/chunks/BGOAb-lb.js
@@ -1,5 +1,1 @@
-import { g as e } from "./KzIwl7iM.js";
-const a = () => "DuckDuckFit", n = (l = {}, t = {}) => (t.locale ?? e()) === "en" ? a() : "app_title";
-export {
-  n as a
-};
+import{g as e}from"./KzIwl7iM.js";const a=()=>"DuckDuckFit",n=(l={},t={})=>(t.locale??e())==="en"?a():"app_title";export{n as a};
