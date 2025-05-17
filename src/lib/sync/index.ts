fae646aa43145bd19adb0cc8b54fb610cceb9d71
Nathan Faucett <nathanfaucett@gmail.com>
2025-05-17 08:55:50 -0400

    remove debug

--- src/lib/sync/index.ts
+++ src/lib/sync/index.ts
@@ -1,8 +1,3 @@
-import { browser } from "$app/environment";
 import { WebRTCClientAdapter } from "./WebRTCClientAdapter";
-
-if (browser) {
-  localStorage.debug = '*'
-}
 
 export const webRTCClientAdapter = new WebRTCClientAdapter();
