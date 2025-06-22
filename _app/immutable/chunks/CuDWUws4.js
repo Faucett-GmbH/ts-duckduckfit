ea53b00dda2d002d35b4d2b44098127e2278c4ac
nathanfaucett <nathanfaucett@users.noreply.github.com>
2025-06-22 15:39:35 +0000

    deploy: 25cc40dc55750059763a0303d0772be28176c44a

---
+++ _app/immutable/chunks/CuDWUws4.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_pyfvh7?.base??"/ts-duckduckfit",t=globalThis.__sveltekit_pyfvh7?.assets??s;export{t as a,s as b};
--- _app/immutable/chunks/D4DfMVmk.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1htpvtq?.base??"/ts-duckduckfit",t=globalThis.__sveltekit_1htpvtq?.assets??s;export{t as a,s as b};
