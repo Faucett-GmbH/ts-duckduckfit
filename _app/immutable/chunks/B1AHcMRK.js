5bf4ea588e7ba1d3ef89019ac91e7f03940a9abb
nathanfaucett <nathanfaucett@users.noreply.github.com>
2025-06-22 11:57:51 +0000

    deploy: 0d8daa7e1af3326ca104f815dfe77050a2e913a8

--- _app/immutable/chunks/B1AHcMRK.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_14i2ztk?.base??"/ts-duckduckfit",t=globalThis.__sveltekit_14i2ztk?.assets??s;export{t as a,s as b};
---
+++ _app/immutable/chunks/BUJ0LRrO.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_19dbaj7?.base??"/ts-duckduckfit",a=globalThis.__sveltekit_19dbaj7?.assets??s;export{a,s as b};
