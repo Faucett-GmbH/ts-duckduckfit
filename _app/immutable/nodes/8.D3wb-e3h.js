b36f1a11b62d4ba85163753d5bf352ab35a0ac09
nathanfaucett <nathanfaucett@users.noreply.github.com>
2025-07-13 18:29:05 +0000

    deploy: 3e133ec180c693d971462f2b0352b466064a08b8

---
+++ _app/immutable/nodes/8.D3wb-e3h.js
@@ -0,0 +1,39 @@
+import"../chunks/NZTpNUN0.js";import{m as P,a as w,b as m,p as E,f as y,d as o,s as h,r as t,c as F,t as k,i as Y,k as ee}from"../chunks/Obog5mpC.js";import{d as ae,s as x}from"../chunks/Cpytdzxm.js";import{i as D}from"../chunks/DDEKEYg1.js";import{I as G,s as J,e as H,i as te}from"../chunks/D1li7sz5.js";import{r as re,e as se,f as Z,i as j,g as q}from"../chunks/BsjOymRs.js";import{a as oe}from"../chunks/CcYk-lMf.js";import{aw as le,ax as ne,ay as ce,az as ie,aA as de,aB as me,aC as fe}from"../chunks/CHxYVYJo.js";import{l as pe}from"../chunks/IVZvQc6H.js";import{s as ve}from"../chunks/Bm3b72qe.js";import{a as ue}from"../chunks/SRDCdzcj.js";import{l as K,s as O,p as _e,r as ge}from"../chunks/CRStQCix.js";import"../chunks/By3bhMHB.js";import{a as he}from"../chunks/BcNb3wH1.js";function be(l,r){const a=K(r,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.525.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */const f=[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"}]];G(l,O({name:"moon"},()=>a,{get iconNode(){return f},children:(n,c)=>{var i=P(),p=w(i);J(p,r,"default",{}),m(n,i)},$$slots:{default:!0}}))}function xe(l,r){const a=K(r,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.525.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */const f=[["circle",{cx:"12",cy:"12",r:"4"}],["path",{d:"M12 2v2"}],["path",{d:"M12 20v2"}],["path",{d:"m4.93 4.93 1.41 1.41"}],["path",{d:"m17.66 17.66 1.41 1.41"}],["path",{d:"M2 12h2"}],["path",{d:"M20 12h2"}],["path",{d:"m6.34 17.66-1.41 1.41"}],["path",{d:"m19.07 4.93-1.41 1.41"}]];G(l,O({name:"sun"},()=>a,{get iconNode(){return f},children:(n,c)=>{var i=P(),p=w(i);J(p,r,"default",{}),m(n,i)},$$slots:{default:!0}}))}var $e=y(`<label class="inline-flex items-center cursor-pointer"><input/> <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div> <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"><!></span></label>`);function ye(l,r){E(r,!0);let a=_e(r,"checked",15,!1),f=ge(r,["$$slots","$$events","$$legacy","children","checked"]);var n=$e(),c=o(n);re(c),se(c,()=>({type:"checkbox",class:"sr-only peer",...f}));var i=h(c,4),p=o(i);ve(p,()=>r.children),t(i),t(n),ue(c,a),m(l,n),F()}const ke=l=>{me(l.currentTarget.value)},we=l=>{fe(l.currentTarget.value)};var Me=y("<option> </option>"),Se=y("<option> </option>"),Te=y('<div class="flex flex-row items-center flex-grow mb-2"><label for="theme" class="me-2"> </label> <!></div> <div class="flex flex-row items-center flex-grow mb-2"><label for="language"> </label> <select name="language" class="ms-2"></select></div> <div class="flex flex-row items-center flex-grow mb-2"><label for="measurementSystem"> </label> <select class="ms-2" name="measurementSystem"></select></div>',1),Ce=y('<form class="card flex flex-col"><h2> </h2> <!></form>');function Ge(l,r){E(r,!0);const a=oe(r.data.settings),f=v=>{de(v.currentTarget.checked?"light":"dark")};var n=Ce(),c=o(n),i=o(c,!0);t(c);var p=h(c,2);{var Q=v=>{var I=Te(),M=w(I),S=o(M),R=o(S,!0);t(S);var U=h(S,2);const V=ee(()=>a.doc.theme==="light");ye(U,{name:"theme",get checked(){return Y(V)},onchange:f,children:(d,s)=>{var e=P(),$=w(e);{var g=b=>{xe(b,{})},z=b=>{be(b,{})};D($,b=>{a.doc.theme==="light"?b(g):b(z,!1)})}m(d,e)},$$slots:{default:!0}}),t(M);var T=h(M,2),C=o(T),W=o(C,!0);t(C);var u=h(C,2);u.__change=[ke],H(u,20,()=>pe,d=>d,(d,s)=>{var e=Me(),$=o(e,!0);t(e);var g={};k(()=>{Z(e,a.doc.locale==s),x($,s),g!==(g=s)&&(e.value=(e.__value=s)??"")}),m(d,e)}),t(u);var L;j(u),t(T);var A=h(T,2),N=o(A),X=o(N,!0);t(N);var _=h(N,2);_.__change=[we],H(_,20,()=>["metric","imperial"],te,(d,s)=>{var e=Se(),$=o(e,!0);t(e);var g={};k(z=>{Z(e,a.doc.measurementSystem==s),x($,z),g!==(g=s)&&(e.value=(e.__value=s)??"")},[()=>le[`${s}_system`]()]),m(d,e)}),t(_);var B;j(_),t(A),k((d,s,e)=>{x(R,d),x(W,s),L!==(L=a.doc.locale)&&(u.value=(u.__value=a.doc.locale)??"",q(u,a.doc.locale)),x(X,e),B!==(B=a.doc.measurementSystem)&&(_.value=(_.__value=a.doc.measurementSystem)??"",q(_,a.doc.measurementSystem))},[()=>ne(),()=>ce(),()=>ie()]),m(v,I)};D(p,v=>{a.doc&&v(Q)})}t(n),k(v=>x(i,v),[()=>he()]),m(l,n),F()}ae(["change"]);export{Ge as component};
