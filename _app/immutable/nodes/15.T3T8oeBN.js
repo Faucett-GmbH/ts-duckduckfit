0a04159b1d33c32be06fa918cbcb4455fa9bb348
nathanfaucett <nathanfaucett@users.noreply.github.com>
2025-07-20 16:24:45 +0000

    deploy: 5a25d868323c4f6f75de06c95d916747382673e2

---
+++ _app/immutable/nodes/15.T3T8oeBN.js
@@ -0,0 +1,20 @@
+import"../chunks/DsnmJJEf.js";import{m as b,a as w,b as u,p as g,f as k,t as f,c as y,$ as E,d as a,s as v,r as s}from"../chunks/DrkzScFF.js";import{h as I,s as N}from"../chunks/jLUO2zaU.js";import{s as j}from"../chunks/By6XpgFE.js";import"../chunks/ciQVMl6O.js";import{L as _}from"../chunks/CCqSeNgW.js";import{b as z}from"../chunks/6RsLnPgm.js";import{E as A}from"../chunks/CkcnRJjc.js";import"../chunks/BFgaWYGP.js";import{I as H,s as L}from"../chunks/B-zhu5u3.js";import{l as M,s as P}from"../chunks/DvX7ffy5.js";function T(l,o){const e=M(o,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.525.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */const i=[["path",{d:"m12 19-7-7 7-7"}],["path",{d:"M19 12H5"}]];H(l,P({name:"arrow-left"},()=>e,{get iconNode(){return i},children:(r,d)=>{var t=b(),m=w(t);L(m,o,"default",{}),u(r,t)},$$slots:{default:!0}}))}var W=k('<div class="container mx-auto p-4"><div class="card flex flex-col"><div class="flex flex-row justify-between"><div class="flex flex-row items-center"><a class="btn icon sm"><!></a> <h3 class="m-0 ms-2"> </h3></div></div> <div class="flex flex-col"><!></div></div></div>');function S(l,o){g(o,!0);var e=W();I(n=>{f(x=>E.title=x,[()=>_()])});var i=a(e),r=a(i),d=a(r),t=a(d),m=a(t);T(m,{}),s(t);var p=v(t,2),h=a(p,!0);s(p),s(d),s(r);var c=v(r,2),$=a(c);A($,{}),s(c),s(i),s(e),f(n=>{j(t,"href",`${z}/workout-templates`),N(h,n)},[()=>_()]),u(l,e),y()}export{S as component};
